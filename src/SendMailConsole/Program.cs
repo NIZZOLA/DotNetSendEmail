c1b46095edd013d640afad82c73e0fc5f2024584
nizzola <marcio@nizzola.com.br>
2021-06-07 22:51:25 -0300

    ajuste de métodos para envio de anexos.

--- src/SendMailConsole/Program.cs
+++ src/SendMailConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
+using System.Linq;
 
 namespace SendMailConsole
 {
@@ -15,8 +17,16 @@ namespace SendMailConsole
 
             var conta = config.GetSection("EmailServerAccount").Get<EmailServerAccount>();
 
-            
-            var sendResponse =  Util.EnviarMensagem(conta, new string[] { "marcio.nizzola@etec.sp.gov.br" } , null, "TESTE DE EMAIL " + DateTime.Now, "teste de email etec ", null);
+
+            // Busca automaticamente todos os arquivos em todos os subdiretórios
+            string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "\\Files";
+            DirectoryInfo Dir = new DirectoryInfo(path);
+            FileInfo[] files = Dir.GetFiles("*", SearchOption.AllDirectories);
+            var fileNames = files.Select(f => f.FullName).ToArray();
+
+            // chamada da função de disparo do e-mail
+            var sendResponse = Util.EnviarMensagem(conta, new string[] { "marcio.nizzola@etec.sp.gov.br" }, null, 
+                                                    "TESTE DE EMAIL " + DateTime.Now, "teste de email etec ", fileNames);
 
             Console.WriteLine(sendResponse);
         }
--- src/SendMailConsole/Util.cs
+++ src/SendMailConsole/Util.cs
@@ -2,111 +2,102 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 
 namespace SendMailConsole
 {
     public static class Util
     {
-        private static void AddBcc(MailMessage message, string[] emailcc)
+        private static void AddEmails(MailAddressCollection emailcollection, string[] emailList)
         {
-            if (emailcc != null)
+            if (emailList != null)
             {
-                foreach (var destinatario in emailcc)
+                foreach (var emailString in emailList)
                 {
-                    if (destinatario != null)
+                    if (emailString != null)
                     {
-                        string[] emaildestino = destinatario.Split(';');
-                        //Destinatário
-                        foreach (string vEmailP in emaildestino)
+                        string[] emailSplited = emailString.Split(';');
+                        foreach (string email in emailSplited)
                         {
-                            message.Bcc.Add(new MailAddress(vEmailP));
+                            emailcollection.Add(new MailAddress(email));
                         }
                     }
                 }
             }
         }
-        private static void AddDestinatario(MailMessage message, string[] destino)
+        
+        private static void AddAttachment(MailMessage message, string[] anexos)
         {
-            foreach (var destinatario in destino)
+            if (anexos.Length > 0)
             {
-                if (destinatario != null)
+                foreach (string file in anexos)
                 {
-                    string[] emaildestino = destinatario.Split(';');
-                    foreach (string vEmailP in emaildestino)
+                    if (file != "")
                     {
-                        message.To.Add(new MailAddress(vEmailP));
+                        if (System.IO.File.Exists(file))   // verifica a existência do arquivo
+                        {
+                            // Cria o anexo para colocar dentro da mensagem
+                            Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
+                            data.Name = System.IO.Path.GetFileName(file);
+                            // Insere as datas no anexo
+                            ContentDisposition disposition = data.ContentDisposition;
+                            disposition.CreationDate = System.IO.File.GetCreationTime(file);
+                            disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
+                            disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+                            // Adiciona o arquivo na mensagem criada
+                            message.Attachments.Add(data);
+                        }
                     }
                 }
             }
         }
 
-        public static string EnviarMensagem(EmailServerAccount conta, string[] destino, string[] emailcc, string mensagem, string titulo, string anexo)
+        public static string EnviarMensagem(EmailServerAccount conta, string[] destino, string[] emailcc, string mensagem, string titulo, string[] anexos)
         {
-            string para = destino[0];
-
-            if (String.IsNullOrEmpty(para)) 
-            {
+            if (String.IsNullOrEmpty(destino[0])) 
                 return "Erro sem e-mail ! Assunto:" + titulo;
-            }
-
+            
             if (conta == null)
                 return "Erro, conta de e-mail não existente !";
 
-            MailMessage message = new MailMessage();
-            message.From = new MailAddress(conta.EmailOrigem, conta.NomeOrigem);
-            message.ReplyToList.Add(new MailAddress(conta.Retorno));
+            MailMessage message = new MailMessage();                                // instancia o objeto da mensagem
+            message.From = new MailAddress(conta.EmailOrigem, conta.NomeOrigem);    // atribui remetente
+            message.ReplyToList.Add(new MailAddress(conta.Retorno));                // atribui e-mail de resposta
 
+            // o config e-mail debug permite enviar para um e-mail de teste configurado no appsettings
+            // caso estejamos rodando o software em modo Teste isto é útil para não disparar para clientes
             if (conta.EmailDebug != "")
-            {
-                para = conta.EmailDebug;
-                message.To.Add(new MailAddress(para));
-            }
+                message.To.Add(new MailAddress(conta.EmailDebug));
             else
             {
-                AddDestinatario(message, destino);
-                AddBcc(message, emailcc);
+                AddEmails(message.To, destino);                                     // adiciona destinatários
+                AddEmails(message.Bcc, emailcc);                                    // adiciona cópias ocultas
             }
-
-            //prioridade do email
-            message.Priority = MailPriority.Normal;
-
-            //utilize true pra ativar html no conteúdo do email, ou false, para somente texto
-            message.IsBodyHtml = false;
-
-            //Assunto do email
-            message.Subject = titulo;
-
-            //corpo do email a ser enviado
-            message.Body = mensagem;
-
-            // Envia a mensagem
-            SmtpClient client = new SmtpClient(conta.Server, conta.Port);
-
-            Boolean ssl = conta.Autentica;
-            client.EnableSsl = ssl;
-
-            // Insere as credenciais se o Servidor SMTP exigir
-            client.Credentials = CredentialCache.DefaultNetworkCredentials;
-
-            //endereço do servidor SMTP(para mais detalhes leia abaixo do código)
-            client.Host = conta.Server;
-
-            //para envio de email autenticado, coloque login e senha de seu servidor de email
-            //para detalhes leia abaixo do código
-            client.Credentials = new NetworkCredential(conta.EmailOrigem, conta.Pass);
+            
+            AddAttachment(message, anexos);                                         // adiciona os anexos se houver
+            message.Priority = MailPriority.Normal;                                 //prioridade do email
+            message.IsBodyHtml = false;                                             //utilize true pra ativar html no conteúdo do email, ou false, para somente texto
+            message.Subject = titulo;                                               //Assunto do email
+            message.Body = mensagem;                                                //corpo do email a ser enviado
+           
+            SmtpClient client = new SmtpClient(conta.Server, conta.Port);           // Instancia o objeto que envia e-mails
+            client.EnableSsl = (bool)conta.Autentica;
+            client.Credentials = CredentialCache.DefaultNetworkCredentials;         // Insere as credenciais se o Servidor SMTP exigir
+            client.Host = conta.Server;                                             //endereço do servidor SMTP(para mais detalhes leia abaixo do código)
+            
+            client.Credentials = new NetworkCredential(conta.EmailOrigem, conta.Pass);  //para envio de email autenticado, coloque login e senha de seu servidor de email
 
             try
             {
+                // realiza o envio da mensagem
                 client.Send(message);
                 return "";
             }
             catch (Exception ex)
             {
-                return " Erro no envio de email para !  " + para + "\r\n" + " " + ex.Message + "      -       " + ex.StackTrace + System.Environment.NewLine;
+                return " Erro no envio de email para !  " + message.To.ToString() + "\r\n" + " " + ex.Message + "      -       " + ex.StackTrace + System.Environment.NewLine;
             }
-
-
         }
     }
 }
